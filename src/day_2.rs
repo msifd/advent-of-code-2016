e09fcca8fd3228aa943074fd66f62aa98032788b
msifeed <remsifeed@gmail.com>
2016-12-04 12:44:22 +0300

    Day 2 - Pt 1

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,11 +1,11 @@
 use std::collections::LinkedList;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Instruction {
     UP, RIGHT, DOWN, LEFT
 }
 
-type Pos = (u8, u8);
+type Pos = (i32, i32);
 type InstrVecs = Vec<LinkedList<Instruction>>;
 
 pub fn parse_input(input: &str) -> InstrVecs {
@@ -22,9 +22,25 @@ pub fn parse_input(input: &str) -> InstrVecs {
     }).collect()
 }
 
-pub fn decode(input: &InstrVecs) -> u16 {
+pub fn decode(input: &InstrVecs) -> i32 {
+    let mut pos = (1, 1);
 
-    0
+    let numbers: Vec<i32> = input.iter().map(|chain| {
+        for inst in chain {
+            use self::Instruction::*;
+
+            if *inst == UP && pos.1 > 0 { pos.1 -= 1 }
+            else if *inst == RIGHT && pos.0 < 2 { pos.0 += 1 }
+            else if *inst == DOWN && pos.1 < 2 { pos.1 += 1 }
+            else if *inst == LEFT && pos.0 > 0 { pos.0 -= 1 }
+        }
+
+        pos.1 * 3 + pos.0 + 1
+    }).collect();
+
+    numbers.iter().enumerate().map(|(i, n)| {
+        n * (10 as i32).pow((numbers.len() - i - 1) as u32)
+    }).sum()
 }
 
 #[test]
